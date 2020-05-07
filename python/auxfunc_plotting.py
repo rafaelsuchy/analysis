57eb60d0b1c06b4f99cc32324ceeaa9b3d70d44d
Rafael Suchy <suchy_rafael@icloud.com>
2020-05-07 16:45:28 +0200

    Auxiliary plotting functions.

---
+++ python/auxfunc_plotting.py
@@ -0,0 +1,36 @@
+""" Module for plotting functions of the auxiliary notebooks """
+
+def choice_patterns(df):
+    """
+    The function plots the choice patterns for a given dataframe of simulated data.
+
+    Input:
+        df: dataframe of simulated / observed data
+
+    Output:
+        Barplot of choices in each period.
+    """
+
+    fig, ax = plt.subplots()
+
+    shares = df_base.groupby("Period").Choice.value_counts(normalize=True).unstack()[
+        ["edu", "a", "b", "home"]
+    ]
+    labs = ["School", "White", "Blue", "Home"]
+
+    shares.plot.bar(stacked=True, ax=ax, width = 0.8)
+
+    ax.legend(
+        labels = labs,
+        loc = "upper center",
+        bbox_to_anchor=(0.5, 1.15),
+        ncol = len(labs)
+    )
+
+    # x-axis options
+    ax.set_xticks(range(0, options["n_periods"], 5))
+    ax.set_xticklabels(range(0, options["n_periods"], 5), rotation="horizontal")
+
+    # y-axis options
+    ax.set_ylim(0,1)
+    ax.set_ylabel("Share of population")
