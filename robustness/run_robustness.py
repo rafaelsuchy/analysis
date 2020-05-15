d83e19623dc49b4e0f6747b10f954b052b27f2b6
Rafael Suchy <suchy_rafael@icloud.com>
2020-05-15 04:40:54 +0200

    Initialization of workflow in run_robustness.py

--- robustness/run_robustness.py
+++ robustness/run_robustness.py
@@ -15,6 +15,7 @@ os.environ.update(parallel_off)
 import pandas as pd
 import numpy as np
 
+from robustness_library import AMBIGUITY_VALUES
 from robustness_library import get_dict_labels  # should enter module auxiliary functions
 from robustness_library import simulation_ambiguity
 from robustness_library import eval_experiece_effect_ambiguity
@@ -29,12 +30,26 @@ NUM_PERIODS = 40
 NUM_AGENTS = 1000
 
 def main():
-    # Need to go through the whole process (saving and recovering data-frames)
 
-    # Core: simulate ambiguity data frames
+    # Core: simulate ambiguity data frames, which are the backbone of the analysis
+    # In the following the implementation from simulation_ambiguity will be done with MPI
+
+    tasks = []
+    for ambiguity_value in AMBIGUITY_VALUES.values():
+        params, _ = rp.get_example_model("kw_94_two")
+        params.loc[("eta", "eta"), "value"] = ambiguity_value
+        params.loc[("eta", "eta"), "comment"] = "value of the ambiguity set"
+        tasks.append(params)
+
+    df_ambiguity = distribute_tasks(simulate, tasks)
+
+    # Expected output: list of dataframes
+
+    # With this list of dataframe we can perform all other tasks
+    # eval_experiece_effect_ambiguity
+    # eval_eu_loss
 
     # Minor: get different functions of all stuff
-
 
 if __name__ == "__main__":
     main()
