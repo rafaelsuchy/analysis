cf570b920b1394ca6486c9654e7bfdbf7aa3538e
Rafael Suchy <suchy_rafael@icloud.com>
2020-05-15 11:57:19 +0200

    Added function to obtain model specification.

--- robustness/run_robustness.py
+++ robustness/run_robustness.py
@@ -12,6 +12,7 @@ parallel_off = {
 }
 os.environ.update(parallel_off)
 
+from functool import partial
 import pandas as pd
 import numpy as np
 
@@ -31,9 +32,17 @@ NUM_AGENTS = 1000
 
 def main():
 
+    # Load the example model
+    # to do: write function for the model specifiction 
+    params, options = rp.get_example_model("kw_94_two")
+
+
+
+
     # Core: simulate ambiguity data frames, which are the backbone of the analysis
     # In the following the implementation from simulation_ambiguity will be done with MPI
 
+    # Create the tasks
     tasks = []
     for ambiguity_value in AMBIGUITY_VALUES.values():
         params, _ = rp.get_example_model("kw_94_two")
@@ -53,3 +62,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+    def example_task(alpha, beta, gamma, x):
+    x_ = np.array(x)
+    result = alpha * (x_[1:] - x_[:-1] ** beta) ** gamma + (1 - x_[:-1]) ** gamma
+
+    return result.sum()
+
+
+if __name__ == "__main__":
+
+    # We fix the details of our evaluation task and draw a sample of evaluation points.
+    num_points, num_inputs = 300, 3
+
+    total_draws = num_inputs * num_points
+    eval_points = np.random.uniform(size=total_draws).reshape(num_points, num_inputs)
+
+    # We partial out all function arguments that remain unchanged.
+    alpha, beta, gamma = 100.0, 2.0, 3.0
+    p_example_task = partial(example_task, alpha, beta, gamma)
+
+    # We specify the amount and type of resources we have available and then start  the parallel
+    # evaluation of our test function.
+    num_proc, is_distributed = 3, True
+    distribute_tasks(p_example_task, eval_points, num_proc, is_distributed)
