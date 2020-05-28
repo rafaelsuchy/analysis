ef7c29c3876595232abbd43f247a113f4566d00c
Rafael Suchy <suchy_rafael@icloud.com>
2020-05-28 18:17:11 +0200

    Test update for both ambiguity values.

--- tests/test_log_likelihood.py
+++ tests/test_log_likelihood.py
@@ -1,48 +1,45 @@
-"""Assert values log-likelihood function under ambiguity"""
+"""Assert values log-likelihood function under ambiguity (speed-up)"""
 import numpy as np
-import pandas as pd
+import pytest
 import respy as rp
-from pandas._testing import assert_frame_equal
 from respy.likelihood import get_crit_func
 from respy.simulate import get_simulate_func
 
-# from respy.tests.utils import process_model_or_seed
-
 
 # Test likelihood function for different values of ambiguity
-# @pytest.mark.parametrize("ambiguity", [0, 0.02])
-# def test_log_likelihood_func(ambiguity):
-def test_log_likelihood_func():
+@pytest.mark.parametrize("ambiguity", [0.00, 0.02])
+def test_log_likelihood_func(ambiguity):
 
-    params, options = rp.get_example_model("kw_94_one", with_data=False)
+    params, options = rp.get_example_model("kw_94_two", with_data=False)
     options["n_periods"] = 10
     options["simulation_agents"] = 200
     options["simulation_seed"] = 999
     options["solution_seed"] = 9999
 
-    # params.loc[("eta", "eta"), "value"] = ambiguity
-    params.loc[("eta", "eta"), "value"] = 0.00
+    params.loc[("eta", "eta"), "value"] = ambiguity
 
     simulate_func = get_simulate_func(params, options)
     df = simulate_func(params)
 
-    loglike_func = get_crit_func(params, options, df, return_comparison_plot_data=True)
-    loglike, df = loglike_func(params)
+    loglike_func = get_crit_func(params, options, df, return_comparison_plot_data=False)
+    loglike = loglike_func(params)
 
-    # Check for expected data format
     assert isinstance(loglike, float)
-    assert isinstance(df, pd.DataFrame)
 
-    # Test with reference simulation
-    df_expected = pd.read_pickle("dfs_loglike_values_absent")
-    assert_frame_equal(df_expected, df)
+    if ambiguity == 0.00:
+        np.testing.assert_almost_equal(
+            loglike,  # actual
+            -78.27710283833025,  # desired (w/o speedup)
+            decimal=5,
+            err_msg="Assertion error log_like_values",
+            verbose=True,
+        )
 
-    log_like_values = pd.read_pickle("loglike_values")
-
-    np.testing.assert_almost_equal(
-        loglike,  # actual
-        log_like_values.loc["absent", 0],  # desired
-        decimal=7,
-        err_msg="Assertion error log_like_values",
-        verbose=True,
-    )
+    elif ambiguity == 0.02:
+        np.testing.assert_almost_equal(
+            loglike,  # actual
+            -79.32695775924732,  # desired (w/o speedup)
+            decimal=5,
+            err_msg="Assertion error log_like_values",
+            verbose=True,
+        )
