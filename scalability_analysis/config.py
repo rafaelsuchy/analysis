e489fba170455be184d3f6f345cd213a36f287d4
Rafael Suchy <suchy_rafael@icloud.com>
2020-06-05 10:03:01 +0200

    Added configuration file.

---
+++ scalability_analysis/config.py
@@ -0,0 +1,11 @@
+"""Configuration for scalability analysis."""
+
+ITERATIONS_THREADS = 100_000
+ITERATIONS_PROCESSES = 100_000
+
+MAX_THREADS = 9
+MAX_PROCESSES = 9
+
+INPUT_DATA_THREADS = "robustness_inputs_kw_94_one.npy"
+INPUT_DATA_PROCESSES = "robustness_inputs_kw_94_one.npy"
+# @ ["robustness_inputs_kw_97_extended.npy", "robustness_inputs_kw_94_one.npy"]
