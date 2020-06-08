2e72f325c8c361897dca3ed6f48aa4f6a5095f32
Rafael Suchy <suchy_rafael@icloud.com>
2020-06-08 22:59:46 +0200

    Explicit period referencing.

--- scalability_analysis/exec_time_scalability.py
+++ scalability_analysis/exec_time_scalability.py
@@ -15,12 +15,12 @@ if SCALABILITY_ANALYSIS == "threads":
             "OMP_NUM_THREADS",
             "OPENBLAS_NUM_THREADS",
             "NUMEXPR_NUM_THREADS",
-            # "VECLIB_MAXIMUM_THREADS",
         ],
         num_threads,
     )
 
     print(update_)
+    print("Number of threads:", num_threads, ".")
     os.environ.update(update_)
 
 import datetime
@@ -28,6 +28,7 @@ import numpy as np
 import pandas as pd
 from config import ITERATIONS
 from config import MAX_THREADS_PROCESSES
+from config import PERIOD
 from respy.solve import _full_solution
 from caller_scalability_analysis import PATH_AUXINPUT_PARAMS
 from caller_scalability_analysis import SCALABILITY_ANALYSIS
@@ -35,7 +36,6 @@ from caller_scalability_analysis import SCALABILITY_ANALYSIS
 
 if __name__ == "__main__":
 
-    # if SCALABILITY_ANALYSIS == "PROCESSES" and len(sys.argv) > 1:
     if SCALABILITY_ANALYSIS == "processes" and len(sys.argv) > 1:
         num_processes = int(sys.argv[1])
 
@@ -53,20 +53,22 @@ if __name__ == "__main__":
             input_params["optim_paras"],
         )
         end = datetime.datetime.now()
+        # print("Iteration", _j)
 
         times.append((end - start).microseconds)
 
     df_times = pd.DataFrame(times, columns=[f"{num_threads}"])
 
     number = MAX_THREADS_PROCESSES[SCALABILITY_ANALYSIS]
-    print(number)
     if int(num_threads) > 1:
         (
             pd.read_pickle(
-                f"./resources/times_df_{SCALABILITY_ANALYSIS}_{number}.pickle"
+                f"./resources/times_df_{number}{SCALABILITY_ANALYSIS}_per{PERIOD}.pickle"
             ).join(df_times, lsuffix="_")
-        ).to_pickle(f"./resources/times_df_{SCALABILITY_ANALYSIS}_{number}.pickle")
+        ).to_pickle(
+            f"./resources/times_df_{number}{SCALABILITY_ANALYSIS}_per{PERIOD}.pickle"
+        )
     else:
         df_times.to_pickle(
-            f"./resources/times_df_{SCALABILITY_ANALYSIS}_{number}.pickle"
+            f"./resources/times_df_{number}{SCALABILITY_ANALYSIS}_per{PERIOD}.pickle"
         )
