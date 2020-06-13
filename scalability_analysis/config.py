49f24c202e59b6d9c232913f4d8708f96c1129f5
Rafael Suchy <suchy_rafael@icloud.com>
2020-06-13 11:45:01 +0200

    Refactored to account for pickle-only data.

--- scalability_analysis/config.py
+++ scalability_analysis/config.py
@@ -5,13 +5,18 @@ ITERATIONS = {"threads": 1_000, "processes": 1_000}
 
 MAX_THREADS_PROCESSES = {"threads": 12, "processes": 12}
 
-INPUT_DATA = Path("./resources/raw_input_data/kw_94_one_input_params.npy")
+INPUT_DATA = Path("./resources/raw_input_data/kw_94_one_input_params.pickle")
 # @[
-#   Path("./resources/raw_input_data/kw_94_one_input_params.npy")
-#   Path("./resources/raw_input_data/kw_97_basic_one_input_params.npy")
-#   Path("./resources/raw_input_data/kw_97_basic_two_input_params.npy")
+#   Path("./resources/raw_input_data/kw_94_one_input_params.pickle")
+#   Path("./resources/raw_input_data/kw_97_basic_one_input_params.pickle")
+#   Path("./resources/raw_input_data/kw_97_basic_two_input_params.pickle")
 # ]
+DATA_FORMAT = "pickle"
+# @["pickle", "npy"]
 
 PERIOD = "per28"
-# @["per1", "per8", "per18", "per28", "per38"] for "kw_94_one_input_params.npy"
-# @["per1", "per8", "per18", "per28", "per38", "per48"] for "kw_97_basic_input_params.npy"
+# @["per1", "per8", "per18", "per28", "per38"] for "kw_94_one_input_params.pickle"
+# for "kw_97_basic_one_input_params.pickle"
+# @["per1", "per8", "per18", "per28", "per38", "per48"]
+# for "kw_97_basic_two_input_params.pickle"
+# @["per1", "per8", "per18", "per28", "per38", "per48"]
